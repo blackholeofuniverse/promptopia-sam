8559fd7dd9490e65149fa6116ce14eba2ae04f02
Samrat <asustufgaming1050@gmail.com>
2024-12-19 16:21:58 +0530

    fix

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,65 +1,10 @@
-"use client";
-
-import React, { Suspense, useState, useEffect } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
-import Form from "@/components/Form";
+import React, { Suspense } from "react";
+import UpdatePromptComp from "@/components/UpdatePromptComp";
 
 const UpdatePrompt = () => {
-  const router = useRouter();
-  const [promptId, setPromptId] = useState(null);
-
-  const searchParams = useSearchParams();
-
-  useEffect(() => {
-    setPromptId(searchParams.get("id"));
-  }, [searchParams]);
-
-  const [post, setPost] = useState({ prompt: "", tag: "" });
-  const [submitting, setIsSubmitting] = useState(false);
-
-  useEffect(() => {
-    const getPromptDetails = async () => {
-      if (promptId) {
-        const response = await fetch(`/api/prompt/${promptId}`);
-        const data = await response.json();
-        setPost({ prompt: data.prompt, tag: data.tag });
-      }
-    };
-
-    getPromptDetails();
-  }, [promptId]);
-
-  const updatePrompt = async (e) => {
-    e.preventDefault();
-    setIsSubmitting(true);
-
-    if (!promptId) return alert("Missing PromptId!");
-
-    try {
-      const response = await fetch(`/api/prompt/${promptId}`, {
-        method: "PATCH",
-        body: JSON.stringify({ prompt: post.prompt, tag: post.tag }),
-      });
-
-      if (response.ok) {
-        router.push("/");
-      }
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setIsSubmitting(false);
-    }
-  };
-
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <Form
-        type="Edit"
-        post={post}
-        setPost={setPost}
-        submitting={submitting}
-        handleSubmit={updatePrompt}
-      />
+      <UpdatePromptComp />
     </Suspense>
   );
 };
